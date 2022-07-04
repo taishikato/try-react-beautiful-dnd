57d45c609a87f8e980eb34f70e390d809ef800b8
taishikato <taishi.k0903@gmail.com>
2022-07-04 12:38:25 -0500

    create dnd list

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import "./App.css";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+const finalSpaceCharacters = [
+  {
+    id: "gary",
+    name: "Gary Goodspeed",
+    thumb: "/images/gary.png",
+  },
+  {
+    id: "cato",
+    name: "Little Cato",
+    thumb: "/images/cato.png",
+  },
+  {
+    id: "kvn",
+    name: "KVN",
+    thumb: "/images/kvn.png",
+  },
+  {
+    id: "mooncake",
+    name: "Mooncake",
+    thumb: "/images/mooncake.png",
+  },
+  {
+    id: "quinn",
+    name: "Quinn Ergon",
+    thumb: "/images/quinn.png",
+  },
+];
+
+function App() {
+  const [characters, setCharacters] = useState(finalSpaceCharacters);
+
+  const handleOnDragEnd = (result) => {
+    if (result.destination === null) return;
+
+    const items = [...characters];
+    const [draggedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, draggedItem);
+
+    setCharacters(items);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Final Space Characters</h1>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId="characters">
+            {(provided) => (
+              <ul
+                className="characters"
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                {characters.map(({ id, name, thumb }, index) => {
+                  return (
+                    <Draggable key={id} draggableId={id} index={index}>
+                      {(provided) => (
+                        <li
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          <div className="characters-thumb">
+                            <img src={thumb} alt={`${name} Thumb`} />
+                          </div>
+                          <p>{name}</p>
+                        </li>
+                      )}
+                    </Draggable>
+                  );
+                })}
+                {provided.placeholder}
+              </ul>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </header>
+      <p>
+        Images from{" "}
+        <a href="https://final-space.fandom.com/wiki/Final_Space_Wiki">
+          Final Space Wiki
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default App;
